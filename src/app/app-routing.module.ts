6b02c78b91070cf8a199d418470d08a622765e3e
uniubeAVA <mobile@uniube.br>
2021-06-02 21:54:38 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'home/:index',
+    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
   },
 ];
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  offset = 0;
+  pokemon = [];
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
+  
+  constructor( private pokeService: PokemonService ) {
 
-  constructor() {}
+  }
 
+  ngOnInit(){
+    this.loadPokemon();
+  }
+
+  loadPokemon( loadMore = false, event? ){
+    if( loadMore ){
+      this.offset += 25;
+    }
+
+    this.pokeService.getPokemon(this.offset).subscribe(res => {
+      console.log('result: ', res);
+      this.pokemon = [...this.pokemon, ...res];
+
+      if(event){
+        event.target.complete();
+      }
+
+      if(this.offset == 125){
+        this.infinite.disabled = true;
+      }
+    })
+  }
+  onSearchChange(e){
+    let value = e.detail.value;
+
+    if( value == '' ){
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+
+    this.pokeService.findPokemon(value).subscribe(res => {
+      this.pokemon = [res];
+    }, err => {
+      this.pokemon = [];
+    })
+  }
 }
